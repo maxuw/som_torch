02a00fc5ce5361a6d56597cbb4ec600c7c75e89b
Plato <m.jackl@student.uw.edu.pl>
2019-06-25 14:31:09 +0200

    added a cycle through rows and move closer function

--- run.py
+++ run.py
@@ -19,12 +19,14 @@ from map_class import MapClass
 
 
 
+#buildings data
+building_sizes = [[0.1, 0.3], [0.1, 0.2], [1., 1.], [0.125, 0.2], [0.529, 0.12], [1.0, 0.3], [0.33, 0.3], 
+                  [0.4, 0.4], [0.67, 0.3], [.33, 0.7], [.5, 0.1]]
+#     torch.rand((10))
+
 #gray colors data
 gray_colors = [[0.1], [0.], [1.], [0.125], [0.529], [1.0], [0.33], [0.4], [0.67], [.33], [.5]]
 #     torch.rand((10))
-
-gray_colors
-
 
 def convert_data(data):
     tensor_data = []
@@ -34,13 +36,17 @@ def convert_data(data):
     return tensor_data
 
 
+def map_view(map_):
+    return map_.view(dim, length, width)
+
+
 # +
 # Network configuration
 length = 4
 width = 3
 number_iterations = 100
 
-input_data = convert_data(gray_colors)
+input_data = convert_data(building_sizes)
 
 
 move_closer_coef = 0.5
@@ -49,8 +55,56 @@ move_closer_coef = 0.5
 #     dim = 1
 # else:
 dim = len(input_data[0])
+
+
 # -
-input_data[0].shape
+def cycle(map_, input_data):
+    for row in input_data:
+        i_bmu = map1.find_bmu(row).item()
+        map_.move_closer(i_bmu, row)
+        
+    print(map_view(map_.map))
+        
+
+
+
+
+
+
+
+map1 = MapClass(length, width, dim, move_closer_coef)
+
+map_view(map1.map)
+
+
+
+cycle(map1, input_data)
+
+
+
+input_data[0]
+
+map1.find_bmu(input_data[0]).item()
+
+map1.move_closer(3, input_data[0])
+
+map_view(map1.map)
+
+input_data[1]
+
+map1.find_bmu(input_data[1])
+
+map1.move_closer(2, input_data[1])
+
+map_view(map1.map)
+
+
+
+
+
+
+
+
 
 torch.FloatTensor([0., 0., 1.])
 
